180c9955a74a1177b34ebf0d95a66f97ab53eac4
Nadine Salem <naesalem@outlook.com>
2025-01-10 19:35:00 -0600

    added ascii gif header to alpha.lua

--- lua/nae/plugins/colorscheme.lua
+++ lua/nae/plugins/colorscheme.lua
@@ -1,11 +1,28 @@
 return {
-	"ptdewey/darkearth-nvim",
-	dependencies = {
-		"rktjmp/lush.nvim",
-	},
+	url = "https://codeberg.org/jthvai/lavender.nvim",
+	branch = "stable", -- versioned tags + docs updates from main
+	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.o.termguicolors = true
-		vim.cmd("colorscheme darkearth")
+		-- Add configuration before setting colorscheme
+		vim.g.lavender = {
+			transparent = {
+				background = false,
+				float = false,
+				popup = false,
+				sidebar = false,
+			},
+			contrast = true,
+			italic = {
+				comments = true,
+				functions = true,
+				keywords = false,
+				variables = false,
+			},
+			signs = false,
+		}
+
+		-- Set the colorscheme
+		vim.cmd("colorscheme lavender")
 	end,
 }
